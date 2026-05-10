f09950305c0376d63d25c45cc7da845ba514b378
YeslieSnayder <kuzmickiy2000@mail.ru>
2026-05-10 23:18:10 +0300

    feat: added logging, error handling, etc.

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,43 @@
 use std::{io::BufRead, io::BufReader, fs::File, path::PathBuf};
+use anyhow::{Result, Context};
 use clap::Parser;
+use log::info;
 
 /// Command line arguments
-#[derive(Parser)]
+#[derive(Parser, Debug)]
 struct Cli {
     /// The pattern to look for
     pattern: String,
     /// The path to a file to read
     path: PathBuf,
+
+    #[command(flatten)]
+    verbosity: clap_verbosity_flag::Verbosity,
 }
 
-fn main() {
+fn main() -> Result<()> {
     let args = Cli::parse();
+    env_logger::Builder::new()
+        .filter_level(args.verbosity.into())
+        .init();
 
-    let file = File::open(&args.path).expect("can't open file");
+    let file = File::open(&args.path).
+        with_context(|| format!("can't open file {:?}", args.path))?;
     let reader = BufReader::new(file);
+    info!("file successfully read");
 
+    // let pb = indicatif::ProgressBar::new_spinner();
+    let mut i: u32 = 0;
     for line in reader.lines().flatten() {
         if line.contains(&args.pattern) {
             println!("{}", line);
+            i += 1;
         }
+        // pb.inc(1);
     }
+    // pb.finish_with_message("done");
+
+    info!("{} matches found", i);
+
+    Ok(())
 }
